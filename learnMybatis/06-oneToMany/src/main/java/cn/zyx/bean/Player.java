b7445488459b781d53760ef197c898c2a35fddc9
Zyx <yixin0226@126.com>
2019-05-28 21:58:53 +0800

    mybatis 一对多

---
+++ learnMybatis/06-oneToMany/src/main/java/cn/zyx/bean/Player.java
@@ -0,0 +1,45 @@
+package cn.zyx.bean;
+
+/**
+ * 球员
+ */
+public class Player {
+    private int id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Player() {
+    }
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public Player(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
