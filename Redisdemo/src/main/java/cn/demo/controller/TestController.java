33bc8bfa48f7d12b7205fc1c1848e3f545552027
Zyx <yixin0226@126.com>
2019-10-08 09:02:23 +0800

    demo for redis

---
+++ Redisdemo/src/main/java/cn/demo/controller/TestController.java
@@ -0,0 +1,24 @@
+package cn.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/redis/demo")
+public class TestController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @RequestMapping("/redis1")
+    public Object redisTest1(){
+        redisTemplate.opsForValue().set("name","pp");
+
+        Object name = redisTemplate.opsForValue().get("name");
+
+        System.out.println(name);
+        return "ss";
+    }
+}
