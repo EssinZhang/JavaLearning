bb26670e286ad5ec57aaa0702842c52f7356fce9
zyx <yixin0226@126.com>
2019-12-19 16:30:36 +0800

    added a jsonutil to project

---
+++ learnSpringCloud/orderlist/src/main/java/cn/zyx/orderlist/service/ProductOrderClient.java
@@ -0,0 +1,11 @@
+package cn.zyx.orderlist.service;
+
+/**
+ * description: ProductOrderClient <br>
+ * date: 2019/12/19 15:22 <br>
+ *
+ * @author: ZhangYixin <br>
+ * version: 1.0 <br>
+ */
+public interface ProductOrderClient {
+}
---
+++ learnSpringCloud/orderlist/src/main/java/cn/zyx/orderlist/utils/JsonUtils.java
@@ -0,0 +1,11 @@
+package cn.zyx.orderlist.utils;
+
+/**
+ * description: JsonUtils <br>
+ * date: 2019/12/19 15:47 <br>
+ *
+ * @author: ZhangYixin <br>
+ * version: 1.0 <br>
+ */
+public class JsonUtils {
+}
