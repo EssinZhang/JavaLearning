9fc05143da6f29f9bd54d4d3834eca631f7a6bd2
zhang <281496104@qq.com>
2020-01-16 17:14:21 +0800

    延时消费 适用于电商未支付订单检验 增加对应延时测试接口

--- learnRocketMQ/rocketmq-demo/src/main/java/cn/zyx/rocketmqdemo/controller/PayController.java
+++ learnRocketMQ/rocketmq-demo/src/main/java/cn/zyx/rocketmqdemo/controller/PayController.java
@@ -12,7 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.tinygroup.springutil.SpringUtil;
+
+import java.util.HashMap;
 
 /**
  * @Description
@@ -46,6 +47,37 @@ public class PayController {
 
         return "hahaha";
 
+    }
+
+    @RequestMapping("/api/v1/delayTest")
+    public Object delayTest(String text ,int delayLevel) throws RemotingException, MQClientException, InterruptedException {
+        Message message = new Message(JmsConfig.TOPIC,"tags",("hello RocketMQ = "+text).getBytes());
+
+        //设置延迟时间等级
+        //1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h
+        HashMap<Integer,String> delayTimeOfLevel = new HashMap<>(18);
+        String longString = "1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h";
+        String[] strings = longString.split(" ");
+        for (int i = 1 ; i <= strings.length ; i++){
+            delayTimeOfLevel.put(i,strings[i-1]);
+        }
+        message.setDelayTimeLevel(delayLevel);
+
+        payProducer.getDefaultMQProducer().send(message, new SendCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                System.out.printf("发送结果: %s /t msg: +%s", sendResult.getSendStatus(),sendResult);
+            }
+
+            @Override
+            public void onException(Throwable e) {
+                e.printStackTrace();
+                //补偿机制，根据业务情况进行使用，看是否进行重试
+            }
+        });
+
+        String returnString = "延时等级："+delayLevel+"\t延时时间："+delayTimeOfLevel.get(delayLevel);
+        return returnString;
     }
 
     @GetMapping("/test")
