dbbff6c801a82fc9bcf00f6297284f559560acd7
zhang <281496104@qq.com>
2020-09-21 16:28:05 +0800

    Optional判断传入对象是否为空的 使用方法

--- jdk8Project/src/chapter1/optional_demo/OptionalDemo01.java
+++ jdk8Project/src/chapter1/optional_demo/OptionalDemo01.java
@@ -13,12 +13,30 @@ import java.util.Optional;
 public class OptionalDemo01 {
 
     public static void main(String[] args) {
-        Person ps = null;
+//        Person ps = new Person();
+//
+//        Optional<Person> ps2 = Optional.of(ps);
+//        System.out.println("ps2"+ps2);
+//
+//        Optional<Person> ps1 = Optional.ofNullable(ps);
+//        if (ps1.isPresent()) {
+//            System.out.println("optinal对象不为空");
+//            System.out.println(ps1.get());
+//            System.out.println(ps1.get().age+"----------"+ps1.get().name);
+//        }else {
+//            System.out.println("optinal对象为空");
+//        }
 
-        //Optional<Person> opt = Optional.of(ps);
+        Person person1 = null;
+        Person personDemo = new Person("xiaogui",12);
 
-        Optional<Person> ps1 = Optional.ofNullable(ps);
-        System.out.println(ps1);
+        Person person = Optional.ofNullable(person1).orElse(personDemo);
+        System.out.println("name:"+person.getName()+";age:"+person.getAge());
+
+        //lambda表达式写法
+        Integer integer = Optional.ofNullable(personDemo).map(obj -> obj.getAge()).orElse(7);
+        System.out.println(integer);
+
     }
 
 }
--- jdk8Project/src/chapter1/optional_demo/Person.java
+++ jdk8Project/src/chapter1/optional_demo/Person.java
@@ -13,6 +13,14 @@ public class Person {
 
     int age;
 
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
