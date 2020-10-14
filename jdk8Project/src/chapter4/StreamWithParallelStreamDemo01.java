d6986f68cf8aa445c95e2f4dd96fa3aca5c01017
zhang <281496104@qq.com>
2020-10-14 14:25:22 +0800

    jdk8中流操作和foreach集合遍历

---
+++ jdk8Project/src/chapter4/StreamWithParallelStreamDemo01.java
@@ -0,0 +1,11 @@
+package chapter4;
+
+/**
+ * description: StreamWithParallelStreamDemo01 <br>
+ * date: 2020/10/13 14:57 <br>
+ *
+ * @author: ZhangYixin <br>
+ * version: 1.0 <br>
+ */
+public class StreamWithParallelStreamDemo01 {
+}
---
+++ jdk8Project/src/chapter4/StreamofForeachDemo01.java
@@ -0,0 +1,11 @@
+package chapter4;
+
+/**
+ * description: StreamofForeachDemo01 <br>
+ * date: 2020/10/14 11:02 <br>
+ *
+ * @author: ZhangYixin <br>
+ * version: 1.0 <br>
+ */
+public class StreamofForeachDemo01 {
+}
