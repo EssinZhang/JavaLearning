de977db5eab4091364fa1beda054595ea2193baf
Zyx <yixin0226@126.com>
2020-10-20 10:34:24 +0800

    summarizing集合统计

--- jdk8Project/src/chapter5/SummarizingDemo01.java
+++ jdk8Project/src/chapter5/SummarizingDemo01.java
@@ -1,7 +1,9 @@
 package chapter5;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * description: SummarizingDemo01 <br>
@@ -16,6 +18,12 @@ public class SummarizingDemo01 {
         List<Student> students = Arrays.asList(new Student("辽宁", 18), new Student("吉林", 19),
                 new Student("辽宁", 23), new Student("吉林", 20),
                 new Student("辽宁", 24), new Student("吉林", 23));
+        IntSummaryStatistics result = students.stream().collect(Collectors.summarizingInt(Student::getAge));
+        System.out.println("人数："+result.getCount());
+        System.out.println("年龄总和："+result.getSum());
+        System.out.println("最小年龄："+result.getMin());
+        System.out.println("最大年龄："+result.getMax());
+        System.out.println("平均年龄："+result.getAverage());
 
     }
 
